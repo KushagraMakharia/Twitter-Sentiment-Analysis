88498d15085460a4e0957c25fe3f3513196deb27
Kushagra Makharia <KushagraMakharia@users.noreply.github.com>
2018-11-01 21:52:18 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -3,28 +3,33 @@ from textblob import TextBlob
 import csv
 
 
-# Step 1 - Authenticate
-consumer_key= 'CONSUMER_KEY'
-consumer_secret= 'CONSUMER_SECRET_KEY'
 
-access_token='ACCESS_TOKEN_KEY'
-access_token_secret='ACCESS_TOKEN_SECRET_KEY'
+
+# Step 1 - Authenticate
+consumer_key= ''  #Comsumer Key
+consumer_secret= '' #Consumer Secret Key
+
+access_token='' #Access Token
+access_token_secret='' #Access Secret Token
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
+
+
+#Step 2 - Function
+def get_label(analysis, threshold = 0):
+	if analysis.sentiment[0]>threshold:
+		return 'Positive'
+	else:
+		return 'Negative'
+
 #Step 3 - Retrieve Tweets
-public_tweets = api.search('Trump')
-infile = 'file.csv'
+public_tweets = api.search('Bohemian Rapsody')
 
 
-
-
-#CHALLENGE - Instead of printing out each tweet, save each Tweet to a CSV file
-#and label each one as either 'positive' or 'negative', depending on the sentiment 
-#You can decide the sentiment polarity threshold yourself
 
 
 for tweet in public_tweets:
@@ -35,14 +40,14 @@ for tweet in public_tweets:
     print(analysis.sentiment)
     print("")
 
-with open(infile, 'r') as csvfile:
-    rows = csv.reader(csvfile)
-    for row in rows:
-        sentence = row[0]
-        blob = TextBlob(sentence)
-        print ('sentence')
-       	if(analysis.sentiment.polarity > 0.5):
-       		print ("positive")
-       	else:
-       		print("negative")
+#Step 4 - Make a csv of output
+sentiment_polarities = []
+with open('Bohemian Rapsody', 'w') as movie_file:
+		movie_file.write('tweet,sentiment_label\n')
+		for tweet in public_tweets:
+			analysis = TextBlob(tweet.text)
+			#Get the label corresponding to the sentiment analysis
+			sentiment_polarities.append(analysis.sentiment[0])
+			movie_file.write('%s,%s\n' % (tweet.text, get_label(analysis)))
+
         	
